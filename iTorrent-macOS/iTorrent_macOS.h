b81437a49b35afcce4243c7d584fa823bcadd28b
XITRIX <xitrix@bk.ru>
2020-07-24 08:47:12 +0300

    Adding catalyst

---
+++ iTorrent-macOS/iTorrent_macOS.h
@@ -0,0 +1,19 @@
+//
+//  iTorrent_macOS.h
+//  iTorrent-macOS
+//
+//  Created by Daniil Vinogradov on 24.07.2020.
+//  Copyright © 2020  XITRIX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iTorrent_macOS.
+FOUNDATION_EXPORT double iTorrent_macOSVersionNumber;
+
+//! Project version string for iTorrent_macOS.
+FOUNDATION_EXPORT const unsigned char iTorrent_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iTorrent_macOS/PublicHeader.h>
+
+
