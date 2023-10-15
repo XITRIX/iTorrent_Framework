76f38dfad28255271e4fb7c906c9f014b589604d
Daniil Vinogradov <xitrix@bk.ru>
2023-10-15 19:09:54 +0200

    XROS support added

--- make.sh
+++ make.sh
@@ -4,6 +4,8 @@ mkdir build
 
 rm -r ./build/ITorrentFramework-iphonesimulator.xcarchive
 rm -r ./build/ITorrentFramework-iphoneos.xcarchive
+rm -r ./build/ITorrentFramework-xrsimulator.xcarchive
+rm -r ./build/ITorrentFramework-xros.xcarchive
 rm -r ./build/ITorrentFramework.xcframework
 
 xcodebuild archive \
@@ -18,10 +20,33 @@ xcodebuild archive \
  -sdk iphoneos \
  SKIP_INSTALL=NO
 
+xcodebuild archive \
+ -scheme ITorrentFramework \
+ -archivePath ./build/ITorrentFramework-xrsimulator.xcarchive \
+ -sdk xrsimulator \
+ SKIP_INSTALL=NO
+
+xcodebuild archive \
+ -scheme ITorrentFramework \
+ -archivePath ./build/ITorrentFramework-xros.xcarchive \
+ -sdk xros \
+ SKIP_INSTALL=NO
+
 xcodebuild -create-xcframework \
  -framework ./build/ITorrentFramework-iphonesimulator.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
  -framework ./build/ITorrentFramework-iphoneos.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
+ -framework ./build/ITorrentFramework-xrsimulator.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
+ -framework ./build/ITorrentFramework-xros.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
  -output ./build/ITorrentFramework.xcframework
 
- rm -r ./build/ITorrentFramework-iphonesimulator.xcarchive
- rm -r ./build/ITorrentFramework-iphoneos.xcarchive
+
+xcodebuild -create-xcframework \
+ -framework ./build/ITorrentFramework-iphonesimulator.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
+ -framework ./build/ITorrentFramework-iphoneos.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
+ -framework ./build/ITorrentFramework-xrsimulator.xcarchive/Products/Library/Frameworks/ITorrentFramework.framework \
+ -output ./build/ITorrentFramework.xcframework
+
+# rm -r ./build/ITorrentFramework-iphonesimulator.xcarchive
+# rm -r ./build/ITorrentFramework-iphoneos.xcarchive
+# rm -r ./build/ITorrentFramework-xrsimulator.xcarchive
+# rm -r ./build/ITorrentFramework-xros.xcarchive
